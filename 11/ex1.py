fa33321ed3d6549f1015cca688744616a236b6d3
Tobias Heukäufer <tobi@tobchen.de>
2017-12-11 08:30:26 +0100

    Solutions for day 11

---
+++ 11/ex1.py
@@ -0,0 +1,37 @@
+import re
+
+def calc_steps(x, y):
+	if abs(x) >= abs(y):
+		return x
+	else:
+		return abs(abs(y) - abs(x)) / 2 + abs(x)
+
+with open('file.txt') as file:
+	data = [x for x in file.read().split(',')]
+
+# data = "ne,ne,s,s".split(',')
+
+furthest = 0
+
+x = y = 0
+for step in data:
+	if step == 'nw':
+		x -= 1
+		y -= 1
+	elif step == 'n':
+		y -= 2
+	elif step == 'ne':
+		x += 1
+		y -= 1
+	elif step == 'se':
+		x += 1
+		y += 1
+	elif step == 's':
+		y += 2
+	elif step == 'sw':
+		x -= 1
+		y += 1
+
+	furthest = max(furthest, calc_steps(x, y))
+
+print(x, ",", y, ", steps:", calc_steps(x, y), ", furthest:", furthest)
